027ec9a101dd1c23882c9718e7d6a402d66de524
kliu3816 <kliu3816@bu.edu>
2025-05-09 05:34:20 -0400

    allow orgin

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ app.add_middleware(TimeoutMiddleware)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["jelly-jelly-app.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
